cdac9bc9c2836d9b24ec040f4fffd4657efec46d
TheXires <62450142+TheXires@users.noreply.github.com>
2020-12-16 21:30:14 +0100

    Bonus Aufgaben

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var max_bezier_depth = 10;    // max recursion depth -> 2^depth segments
+var max_bezier_depth = 11;    // max recursion depth -> 2^depth segments
 var num_points = 4;          // number of control/input point
 var CP = Array(num_points);
 var line_width = 4;
 var point_size = 10;
-var back_color = '#ffffff';
+var back_color = '#f0f0f0';
 var line_color = '#ff0000';
 var point_color = '#000000';
 
@@ -12,6 +12,11 @@ var ctx;
 
 window.addEventListener('load', function () {
   canvas = document.getElementById('beziers');
+  //bonus aufgabe listener registrieren
+  canvas.onmousedown = canvasDown;
+  canvas.onmouseup = canvasUp;
+  canvas.onmousemove = canvasMove;
+  canvas.oncontextmenu = canvasOnRightClick;
   // check for browser support
   if (canvas && canvas.getContext) {
     canvas.width = document.body.clientWidth;
@@ -45,28 +50,33 @@ function randomize() {
 }
 
 
+let debounceTimestamp;
+
 // Aufgabe 3
 function drawVisualization() {
-  let stepI = document.getElementById('stepI').value;
-  draw();
-  for (var j = 0; j < CP.length - 1; j++) {
-    ctx.setLineDash([15, 5]);
-    ctx.strokeStyle = '#000000';
-    line(CP[j], CP[j + 1]);
-  }
+  if (debounceTimestamp === undefined || ((new Date().getTime()) - debounceTimestamp) > 20) {
+    debounceTimestamp = new Date().getTime();
 
-  var currentLayerOfPoints = CP;
-  do {
-    currentLayerOfPoints = computeNextLayer(currentLayerOfPoints, stepI);
-    for (var j = 0; j < currentLayerOfPoints.length - 1; j++) {
-      ctx.setLineDash([1, 0]);
-      ctx.strokeStyle = '#00ff00';
-      point(currentLayerOfPoints[j]);
-      line(currentLayerOfPoints[j], currentLayerOfPoints[j + 1]);
+    let stepI = document.getElementById('stepI').value;
+    draw();
+    for (var j = 0; j < CP.length - 1; j++) {
+      ctx.setLineDash([15, 5]);
+      ctx.strokeStyle = '#000000';
+      line(CP[j], CP[j + 1]);
     }
-    point(currentLayerOfPoints[currentLayerOfPoints.length - 1]);
-  } while (currentLayerOfPoints.length > 1);
 
+    var currentLayerOfPoints = CP;
+    do {
+      currentLayerOfPoints = computeNextLayer(currentLayerOfPoints, stepI);
+      for (var j = 0; j < currentLayerOfPoints.length - 1; j++) {
+        ctx.setLineDash([1, 0]);
+        ctx.strokeStyle = '#00ff00';
+        point(currentLayerOfPoints[j]);
+        line(currentLayerOfPoints[j], currentLayerOfPoints[j + 1]);
+      }
+      point(currentLayerOfPoints[currentLayerOfPoints.length - 1]);
+    } while (currentLayerOfPoints.length > 1);
+  }
 }
 
 function draw() {
@@ -134,4 +144,80 @@ function computeNextLayer(points, stepI) {
 
 function calc(p1, p2, stepI) {
   return new P(((1 - stepI) * p1.x + stepI * p2.x), ((1 - stepI) * p1.y + stepI * p2.y));
+}
+
+
+//bonus
+
+let dragInProgress = false;
+let draggedPointIndex;
+
+function canvasDown(event) {
+  if (event.which === 1) {
+    const pointResult = calculateNearestPoint(calcMousePositionOnCanvas(event));
+    if (pointResult.distance < 10) {
+      dragInProgress = true;
+      draggedPointIndex = CP.findIndex(e => (e.x === pointResult.point.x && e.y === pointResult.point.y));
+      canvas.style.cursor = "grabbing";
+    }
+  }
+}
+
+function canvasUp(event) {
+  dragInProgress = false;
+  canvas.style.cursor = "default";
+}
+
+function canvasOnRightClick(event) {
+  const pointResult = calculateNearestPoint(calcMousePositionOnCanvas(event));
+  if (pointResult.distance < 10) {
+    event.preventDefault();
+    let clickedPointIndex = CP.findIndex(e => (e.x === pointResult.point.x && e.y === pointResult.point.y));
+    if (CP.length > 2) {
+      CP = CP.filter(e => e !== CP[clickedPointIndex]);
+      num_points--;
+      drawVisualization();
+    }
+  } else {
+    event.preventDefault();
+    CP[CP.length] = calcMousePositionOnCanvas(event);
+    num_points++;
+    drawVisualization();
+  }
+}
+
+
+
+function canvasMove(event) {
+  if (!dragInProgress) {
+    if (calculateNearestPoint(calcMousePositionOnCanvas(event)).distance < 10) {
+      canvas.style.cursor = "grab";
+    } else {
+      canvas.style.cursor = "default";
+    }
+  }
+
+  if (dragInProgress) {
+    CP[draggedPointIndex] = calcMousePositionOnCanvas(event);
+    drawVisualization();
+  }
+}
+
+function calcMousePositionOnCanvas(event) {
+  const canvasRect = canvas.getBoundingClientRect();
+  const x = event.clientX - canvasRect.left;
+  const y = event.clientY - canvasRect.top;
+  return new P(x, y);
+}
+
+function calculateNearestPoint(mouseCoords) {
+  const distances = [];
+  CP.forEach((currentPoint, index) => {
+    distances[index] = {
+      distance: Math.sqrt(Math.pow(mouseCoords.x - currentPoint.x, 2) + Math.pow(mouseCoords.y - currentPoint.y, 2)),
+      point: new P(currentPoint.x, currentPoint.y)
+    };
+  });
+  distances.sort((a, b) => a.distance - b.distance);
+  return distances[0];
 }
